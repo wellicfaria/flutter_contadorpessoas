2a09414a738d0241c4e057e6fd7382cbf09cd178
Wellington Faria <wellicfaria@gmail.com>
2020-06-22 18:49:07 -0300

    final primeira aula

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _pessoas = 0;
+  String _info_pessoas;
+
+  void _outInfoPeople(){
+    String r="";
+    if (_pessoas > 10){
+      r = "Lotado!!!";
+    }else{
+      r= "Pode Entrar!";
+    }
+
+    setState(() {
+      _info_pessoas=r;
+    });
+  }
+
+  void _changePeople(int i){
+    setState(() {
+      _pessoas+=i;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -22,7 +46,7 @@ class _HomeState extends State<Home> {
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("Pessoas 0",
+            Text("Pessoas $_pessoas",
                 style: TextStyle(
                     color: Colors.white, fontWeight: FontWeight.bold)),
             Row(
@@ -37,6 +61,8 @@ class _HomeState extends State<Home> {
                         ),
                         onPressed: () {
                           debugPrint('+1');
+                          _changePeople(1);
+                          _outInfoPeople();
                         })),
                 Padding(
                     padding: EdgeInsets.all(10),
@@ -47,10 +73,12 @@ class _HomeState extends State<Home> {
                         ),
                         onPressed: () {
                           debugPrint('-1');
+                          _changePeople(-1);
+                          _outInfoPeople();
                         }))
               ],
             ),
-            Text("Pode Entrar!",
+            Text("$_info_pessoas",
                 style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
@@ -61,3 +89,4 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
